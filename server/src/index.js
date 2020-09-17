a07d88370861f7eddb2f4fb628d5694c33776c79
Shai Gilboa <shizel@gmail.com>
2020-09-17 18:18:48 -0400

    ready to test signup

--- server/src/index.js
+++ server/src/index.js
@@ -6,52 +6,17 @@ const { GraphQLServer } = require('graphql-yoga');
   //  npx prisma studio --experimental
 const { PrismaClient } = require('@prisma/client')
 
-// the actual implementation of the schema - structure is identical to the schema
-    // TODO: I dont know how to type this
-const resolvers = {
-  Query: {
-    // info: () => null
-    info: () => `This is the API of a Hackernews Clone`, // this is a Scalar type - a leaf in the tree
-    feed: async (parent, args, context, info) =>{
-      // accessing the DB through the Prisma client
-      return context.prisma.link.findMany()
-    },
-    // link: (id: number) => links[id]
-  },
-  Mutation: {
-    post: (parent, args, context, info) => {
-      const newLink  = context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description
-        },
-      })
-      return newLink
-    },
-    updateLink: (parent, args) => {
-      // links[args.id] = {
-      //   ...args
-      // }
-      return args
-    },
-    deleteLink: (parent, args) => {
-      // const outedLink : LinkType | undefined = links.find((link : LinkType) => link.id===args.id)
-      // links = links.filter((link: LinkType) => link.id!==args.id)
-      // return outedLink
-    }
-  },
 
-  /*
-    -----no need because it is straight forward 
-  */
-  // Link: {
-  //   /* as this is a resolver IN a resolver, it is passed 4 arguments:
-  //       root|parent - the result of the previous resolver,
-  //   */
-  //   id: (parent : LinkType ) => parent.id,
-  //   description: (root : LinkType ) => root.description,
-  //   url: (parentORroot : LinkType ) => parentORroot.url,
-  // }
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link
 }
 
 // Bundling both to create a server (from grapql-yoga) What API operation, and how to resolve them
