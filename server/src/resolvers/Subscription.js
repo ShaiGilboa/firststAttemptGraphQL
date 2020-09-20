0497138906c771199c16d26f2c9c8d17581adba9
Shai Gilboa <shizel@gmail.com>
2020-09-19 20:53:27 -0400

    subscription - done

--- server/src/resolvers/Subscription.js
+++ server/src/resolvers/Subscription.js
@@ -2,6 +2,10 @@ const newLinkSubscribe = (parent, args, context, info) => {
   return context.pubsub.asyncIterator("NEW_LINK")
 }
 
+const newVoteSubscribe = (parent, args, context, info) => {
+  return context.pubsub.asyncIterator("NEW_VOTE")
+}
+
 const newLink = {
   subscribe: newLinkSubscribe,
   resolve: payload => {
@@ -9,6 +13,14 @@ const newLink = {
   },
 }
 
+const newVote = {
+  subscribe: newVoteSubscribe,
+  resolve: payload => {
+    return payload
+  }
+}
+
 module.exports = {
   newLink,
+  newVote,
 }
